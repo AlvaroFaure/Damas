e80178e861a77a2c94fc6dc2a210b976b7a7050d
amir haddouch <amerurika@gmail.com>
2016-12-08 18:45:44 +0100

    añadido javadoc coordenada

--- Damas/src/damas/Coordenada.java
+++ Damas/src/damas/Coordenada.java
@@ -6,49 +6,65 @@
 package damas;
 
 /**
+ *La clase Coordenada crea una coordenada a partir de dos valores x e y, establece los valores de la coordenada a partir de otra,
+ *establece cada componente de la coordenada a partir de otra y devuelve el valor de x,y o de la coordenada 
  *
- * @author inftel02
+ * @author Alecia Franco, Alvaro Garcia, Amir Haddouch, Rafael Hidalgo
  */
 public class Coordenada {
-    
+    /**coordenada x*/
     private int x;
+    /**coordenada y*/
     private int y;
-    
+    /**crea una nueva instancia de Coordenada inicializando 'x' e 'y' a 0
+     */
     public Coordenada(){
         x=0; y=0;
     }
-    
+    /**crea una nueva instancia de Coordenada recibiendo una coordenada
+     * @param cord contiene los valores de x e y
+     */
     public Coordenada(Coordenada cord){
         x=cord.x();
         y=cord.y();
     }
-    
+    /**crea una nueva instancia de Coordenada recibiendo dos enteros
+     * @param x valor de la coordenada x
+     * @param y valor de la coordenada y
+     */
     public Coordenada(int x1, int y1){
         x=x1; y=y1;
     }
-    
+    /**
+     *@return devuelve coordenada x
+     */
     public int x(){
         return x;
     }
-    
+    /**
+     *@return devuelve coordenada y
+     */
     public int y(){
         return y;
     }
 
     /**
-     * @param x the x to set
+     * @param x establece el valor de la coordenada x 
      */
     public void setX(int x) {
         this.x = x;
     }
 
     /**
-     * @param y the y to set
+     * @param y establece el valor de la coordenada y 
      */
     public void setY(int y) {
         this.y = y;
     }
     
+    /**
+     *@return devuelve un string de la coordenada de la forma (x,y)
+     */
     public String toString(){
         return "("+ x + ", " + y + ")";
     }
